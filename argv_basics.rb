4b9956917f6d34cb3471dcefc4c0180331996b89
ks2355244 <ks2355244@yahoo.com>
2016-06-15 09:43:31 +0800

    1st edit

--- argv_basics.rb
+++ argv_basics.rb
@@ -1,1 +1,21 @@
 # Place your solutions here
+class RPNCalculator
+    def evaluate(expression)
+        expression = expression.split
+        operands = []
+        evaluation = []
+
+        expression.each do |x|
+            case x
+                when /\d/
+                    evaluation.push(x.to_f)
+                when "-", "/", "*", "+", "**"
+                    operands = evaluation.pop(2)
+                    evaluation.push(operands[0].send(x, operands[1]))
+            end
+        end
+        puts evaluation(3 + 4)
+    end
+end
+
+puts ARGV.join('') + "ay"
